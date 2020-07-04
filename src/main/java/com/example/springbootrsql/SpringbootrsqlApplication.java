64d5f73da4ab01e3118e8450d7884517e7fd1e65
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2020-07-04 15:53:49 -0300

    Spring initializr

---
+++ src/main/java/com/example/springbootrsql/SpringbootrsqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootrsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootrsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootrsqlApplication.class, args);
+	}
+
+}
